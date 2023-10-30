fe7289d8947d1bd9c044bd42f63b85d33a63c882
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-10-30 16:37:39 +0200

    update

--- Keywords/src/Challenge.java
+++ Keywords/src/Challenge.java
@@ -1,6 +1,17 @@
 public class Challenge {
     public static void main(String[] args) {
 
+        int position1 = getPosition(1500);
+        int position2 = getPosition(1000);
+        int position3 = getPosition(500);
+        int position4 = getPosition(100);
+        int position5 = getPosition(25);
+
+        displayScore("ahmed", position1);
+        displayScore("Mohammed", position2);
+        displayScore("adam", position3);
+        displayScore("Rana", position4);
+        displayScore("Hana", position5);
     }
 
     public static int getPosition(int score)
@@ -17,7 +28,7 @@ public class Challenge {
 
     public static void displayScore(String userName , int position )
     {
-        System.out.print("the position of player" + userName + "is: " + position);
+        System.out.println("the position of player " + userName + " is: " + position);
 
     }
 }
