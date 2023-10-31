45c68892a4df4b61404774dd2b5acf6548a837a6
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-10-31 03:56:45 +0200

    update

---
+++ Keywords/src/ForLoop.java
@@ -0,0 +1,12 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        double money = 100.00;
+
+        for(double rate = 7.5; rate <= 10; rate+=0.25)
+        {
+            money+= money*(rate/100);
+            System.out.println("Money is: " + money + " and Rate is: " + rate);
+        }
+    }
+
+}
