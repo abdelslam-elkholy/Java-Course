dfc07ce5bee63ea9a5c4c15ef0bf1c937382d7f0
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-02 09:06:55 +0200

    update

--- Keywords/src/IsSharedDigit.java
+++ Keywords/src/IsSharedDigit.java
@@ -1,3 +1,40 @@
 public class IsSharedDigit {
 
+    public static void main(String[] args) {
+System.out.println(        hasSharedDigit(25 , 67));
+        System.out.println(     hasSharedDigit(18 , 85));
+    }
+
+    public static boolean  hasSharedDigit(int first , int second)
+    {
+        if(first < 10 || first > 99 || second < 10 || second > 99)
+        {
+            return false;
+        }
+
+
+        int fir = first;
+
+        while(fir > 0)
+        {
+            int firDigit = fir %10;
+            int sec = second;
+
+            while(sec > 0)
+            {
+                int secDigit = sec % 10;
+
+                if(firDigit == secDigit)
+                {
+                    return true;
+                }
+
+                sec/=10;
+            }
+            fir/=10;
+        }
+
+
+        return false;
+    }
 }
