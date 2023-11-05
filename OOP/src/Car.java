e617de0c9b1a9a6e28442949418387e095a73984
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-05 08:56:35 +0200

    update

--- OOP/src/Car.java
+++ OOP/src/Car.java
@@ -23,7 +23,39 @@ public class Car {
 private String make ="Tesla";
 private String color = "White";
 private int doors = 4;
-private boolean convertable = true;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setMake(String make) {
+        if(make == null)
+        {
+            this.make = "Unknown";
+        }
+        String lowercaseMake = make.toLowerCase();
+        switch (lowercaseMake)
+        {
+            case  "tesla" , "honda" , "porche" -> this.make = make;
+            default ->
+                this.make = "Unsupported";
+        }
+        this.make = make;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertable(boolean convertable) {
+        this.convertable = convertable;
+    }
+
+    private boolean convertable = true;
 
 public  void descipe() {
     System.out.println(doors + "-Door "+
