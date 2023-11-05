36323b3ca86bd053a773477f7a67780b93571f69
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-05 23:14:24 +0200

    update

--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -2,15 +2,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Car car = new Car();
-        car.setMake("hey");
-        System.out.println(car.getMake());
+//        Car car = new Car();
+//        car.setMake("hey");
+//        System.out.println(car.getMake());
+//
+//        car.setMake(null);
+//        System.out.println(car.getMake());
+//
+//        car.setMake("Honda");
+//        System.out.println(car.getMake());
+//        car.descipe();
 
-        car.setMake(null);
-        System.out.println(car.getMake());
 
-        car.setMake("Honda");
-        System.out.println(car.getMake());
-        car.descipe();
+        BankAccount acc = new BankAccount();
+        acc.setAccountNumber(2023);
+        acc.setAccountBalance(500);
+        acc.setEmail("abdelslam.elkholy@bank.com");
+        acc.setCustomerName("Abdelslam");
+        acc.setPhoneNumber("01020597823");
+
+        System.out.println("Account Info: " +
+                "- " + acc.getAccountNumber()+
+                "- " + acc.getCustomerName()+
+                "- " + acc.getAccountBalance()+
+                "- " + acc.getEmail()+
+                "- " + acc.getPhoneNumber());
+
+        acc.deposite(-5);
+
     }
 }
