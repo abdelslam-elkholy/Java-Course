20fe1d1aed22982a7b1eca3d269baef34140057a
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-05 08:14:00 +0200

    update

--- OOP/src/Car.java
+++ OOP/src/Car.java
@@ -1,12 +1,32 @@
 public class Car {
-private String model;
-private String make;
-private String color;
-private int doors;
-private boolean convertable;
+    public String getModel() {
+        return model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertable() {
+        return convertable;
+    }
+
+    private String model = "Model-x";
+private String make ="Tesla";
+private String color = "White";
+private int doors = 4;
+private boolean convertable = true;
 
 public  void descipe() {
-    System.out.println(doors + "-Door "
+    System.out.println(doors + "-Door "+
             color + " " +
             make + " " +
             model + " " +
--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.descipe();
+    }
 }
