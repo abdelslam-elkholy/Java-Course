1f1603cf1fb274ab93451cbe154626e12b9ddf05
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-06 21:45:48 +0200

    update

---
+++ OOP/src/Worker.java
@@ -0,0 +1,18 @@
+public class Worker {
+    private String name;
+    private String birthDate;
+    private String endDate;
+
+    public Worker(String name, String birthDate, String endDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.endDate = endDate;
+    }
+
+    public int getAge(int currentDate)
+    {
+        return currentDate - Integer.parseInt(birthDate);
+    }
+
+
+}
