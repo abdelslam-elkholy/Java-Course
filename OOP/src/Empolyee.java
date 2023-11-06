abb12f2fcb4bdb96ef9f7cb60981bbf474b79a93
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-06 23:19:41 +0200

    update

---
+++ OOP/src/Empolyee.java
@@ -0,0 +1,12 @@
+public class Empolyee extends Worker {
+    private long empolyeeId ;
+    private String hireDate;
+
+    public Empolyee(String name, String birthDate, String endDate, long empolyeeId, String hireDate) {
+        super(name, birthDate, endDate);
+        this.empolyeeId = empolyeeId;
+        this.hireDate = hireDate;
+    }
+
+
+}
