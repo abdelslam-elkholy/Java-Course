07d28700e30e1d7f986976fd553112a45777026b
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-09 12:26:37 +0200

    encapsulation

--- encapsulation/src/Main.java
+++ encapsulation/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        Printer printer = new Printer(100 , true);
+        printer.printPages(5);
+       System.out.println( printer.getPagesprinted());
+        printer.printPages(8);
+        System.out.println( printer.getPagesprinted());
     }
 }
