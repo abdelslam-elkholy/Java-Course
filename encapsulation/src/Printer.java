14d66b51c0483e420c0389cb1b6578ee52349940
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-09 12:24:17 +0200

    encapsulation

---
+++ encapsulation/src/Printer.java
@@ -0,0 +1,44 @@
+public class Printer {
+    private int tonerLevel;
+    private int pagesprinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel,  boolean duplex) {
+        this.tonerLevel = tonerLevel < 0 ? 0 :Math.min(tonerLevel, 100);
+        this.pagesprinted =0;
+        this.duplex = duplex;
+    }
+
+    public Printer() {
+        this(100 ,  false);
+    }
+
+    public int getTonerLevel() {
+        return tonerLevel;
+    }
+
+    public int setTonerLevel(int tonerLevel) {
+     if (tonerLevel < 0 || tonerLevel > 100)
+     {
+         return  -1;
+     }
+     return this.tonerLevel = tonerLevel;
+    }
+
+    public int getPagesprinted() {
+        return pagesprinted;
+    }
+
+    public int printPages(int pages)
+    {
+        int sheets = duplex ? pages / 2 : pages;
+        sheets = duplex && pages % 2 != 0 ? sheets+1 : sheets;
+        pagesprinted+=sheets;
+        System.out.println("Number Of Pages printed is : " + sheets +" and the printer is duplix");
+        return sheets;
+    }
+
+
+
+
+}
