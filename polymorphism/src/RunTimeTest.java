de31e02b085e5984b2fbc28ad1dd7b889d3e7157
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-10 07:13:26 +0200

    Polymorphism

--- polymorphism/src/RunTimeTest.java
+++ polymorphism/src/RunTimeTest.java
@@ -1,2 +1,14 @@
 public class RunTimeTest {
+    public static void main(String[] args) {
+        Object unkownType = Movie.getMovie("c" , "Star wars");
+        if(unkownType.getClass().getSimpleName() == "Adventure")
+        {
+            Adventure a = (Adventure) unkownType;
+                    a.watchAdventure();
+        } else if (unkownType instanceof Comedy) {
+            ((Comedy) unkownType).watchComedy();
+        } else if (unkownType instanceof ScienceFiction scy) {
+            scy.watchScienceFiction();
+        }
+    }
 }
