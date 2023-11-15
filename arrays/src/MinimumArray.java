fe3a4d062a159fd367f5a4218e246e6ec1f22305
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-15 18:40:28 +0200

    Arrays

--- arrays/src/MinimumArray.java
+++ arrays/src/MinimumArray.java
@@ -3,23 +3,35 @@ import java.util.Scanner;
 
 public class MinimumArray {
     public static void main(String[] args) {
-    readIntegers();
+
     }
 
-    public static void readIntegers()
+    public static int getLength()
     {
+        int length = 0;
         Scanner scanner = new Scanner(System.in);
-do {
- try {
-     System.out.print("Enter Your Array Length: ");
-     int num = scanner.nextInt();
-     System.out.println(num);
-     break;
- }catch (InputMismatchException e)
- {
-     System.out.println("Enter Valid Number");
-     scanner.nextLine();
- }
-}while (true);
+        do {
+            try {
+                 System.out.print("Enter Your Array Length: ");
+                 int num = scanner.nextInt();
+                 System.out.println(num);
+                 break;
+
+             }catch (InputMismatchException e)
+                 {
+                     System.out.println("Enter Valid Number");
+                     scanner.nextLine();
+                 }
+        }while (true);
+                   return length;
     }
+    public static int[] readIntegers(int length)
+    {
+             Scanner scanner = new Scanner(System.in);
+
+    }
+
+
+
 }
+
