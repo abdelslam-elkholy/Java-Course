dfa3a70380f1fc4c756255adf3745065b94d392e
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-15 09:30:23 +0200

    Arrays

--- arrays/src/MinimumArray.java
+++ arrays/src/MinimumArray.java
@@ -1,2 +1,25 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class MinimumArray {
+    public static void main(String[] args) {
+    readIntegers();
+    }
+
+    public static void readIntegers()
+    {
+        Scanner scanner = new Scanner(System.in);
+do {
+ try {
+     System.out.print("Enter Your Array Length: ");
+     int num = scanner.nextInt();
+     System.out.println(num);
+     break;
+ }catch (InputMismatchException e)
+ {
+     System.out.println("Enter Valid Number");
+     scanner.nextLine();
+ }
+}while (true);
+    }
 }
