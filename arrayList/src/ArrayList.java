e4b2d0aa56b4c7948762ff30b15b4b572e389aa5
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-16 14:52:24 +0200

    Array List

---
+++ arrayList/src/ArrayList.java
@@ -0,0 +1,10 @@
+
+import java.util.Arrays;
+
+public class ArrayList {
+
+    public static void main(String[] args) {
+        int [][] towDimensioalArray = new int[3][2];
+        System.out.println(Arrays.deepToString(towDimensioalArray));
+    }
+}
