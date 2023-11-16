eec5b30e98679a4a0c0a50885e73ae91cbaa5392
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-16 21:10:40 +0200

    Array List

---
+++ arrayList/src/GrocerryList.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GrocerryList {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        do {
+            System.out.print("""
+                    Available Actions:
+                    0 - To Shut Down
+                    1 - To Add Item(S) to List
+                    2 - To Remove Items From List
+                    Enter A number For Which Action you want to do: """);
+
+
+           int choose = Integer.parseInt(scanner.nextLine());
+           if (choose ==0) break;
+           else if (choose == 1) {
+               System.out.print("Enter Your Items Seperated By Comma: ");
+               String items = scanner.nextLine();
+               String[] itemsArray = items.split(",");
+               for (String item : itemsArray)
+               {
+                    if(!arrayList.contains(item))
+                    {
+                        arrayList.add(item);
+                    }
+               }
+
+               System.out.println(arrayList);
+
+
+           } else if (choose == 2) {
+               System.out.print("Enter Your Items Seperated By Comma: ");
+               String items = scanner.nextLine();
+               String[] itemsArray = items.split(",");
+               for(String item : itemsArray)
+               {
+                   arrayList.remove(item);
+               }
+
+               System.out.println(arrayList);
+           }
+
+        }while (true);
+
+    }
+
+
+}
