a556ad1eb82f8395099456346f3a7d0fc53ef214
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-17 08:03:28 +0200

    Array List

--- arrayList/src/Mobile.java
+++ arrayList/src/Mobile.java
@@ -1,2 +1,25 @@
 public class Mobile {
+
+
+}
+class Contact
+{
+    private String name;
+    private String phoneNum;
+
+    Contact(String name , String phoneNum)
+    {
+        this.name=name;
+        this.phoneNum=phoneNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+
 }
