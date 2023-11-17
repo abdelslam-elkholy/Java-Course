2a47c741321c002298b68aac1285bd8087a179d1
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-17 08:24:36 +0200

    Array List

--- arrayList/src/Mobile.java
+++ arrayList/src/Mobile.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+
 public class Mobile {
 
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
 
+    Mobile(String myNumber)
+    {
+        this.myNumber = myNumber;
+        myContacts = new ArrayList<>();
+    }
+
+    private int findContact(Contact contact  )
+    {
+        return myContacts.indexOf(contact);
+    }
+
+    private int findContact(String name)
+    {
+
+    }
 }
 class Contact
 {
@@ -21,5 +40,8 @@ class Contact
         return phoneNum;
     }
 
-
+    public static Contact createContact(String name , String phoneNum)
+    {
+        return new Contact(name , phoneNum);
+    }
 }
