6f3881ff86eaa53e0508fc0616754a1913e8bda7
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-18 10:33:41 +0200

    Linked List

---
+++ arrayList/src/LinkedListClass.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class LinkedListClass {
+    public static void main(String[] args) {
+
+//    LinkedList<String> linkedList = new LinkedList<>();
+    var places = new LinkedList<String>();
+     places.add("Cairo");
+     places.add("Alex");
+     System.out.println(places);
+    }
+}
