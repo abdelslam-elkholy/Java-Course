b5202ae0380570cc61192f199057d6a258eb7b0e
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-18 10:44:01 +0200

    Linked List

--- arrayList/src/LinkedListClass.java
+++ arrayList/src/LinkedListClass.java
@@ -8,5 +8,19 @@ public class LinkedListClass {
      places.add("Cairo");
      places.add("Alex");
      System.out.println(places);
+
+     places.addLast("Shebeen");
+     places.addFirst("Meno");
+     System.out.println(places);
+
+     places.offer("Place 1");
+     places.offerFirst("Place offer first");
+     places.offerLast("Place offer last");
+     System.out.println(places);
+
+     places.push("Stack Place Push");
+     System.out.println(places);
+
+
     }
 }
