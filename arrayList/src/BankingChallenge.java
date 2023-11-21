cf8df9e1f450bdc16937b088e0011dcd960fc32a
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-21 13:29:35 +0200

    Boxing and Unboxing

--- arrayList/src/BankingChallenge.java
+++ arrayList/src/BankingChallenge.java
@@ -28,3 +28,37 @@ class Customer
         transactions.add(transaction);
     }
 }
+
+class Bank
+{
+    String name;
+    ArrayList<Customer> customers;
+
+    Bank(String name)
+    {
+        this.name=name;
+        customers=new ArrayList<>();
+
+    }
+
+    public void addNewCustomer(Customer customer)
+    {
+        if (!customers.contains(customer))
+        {
+            customers.add(customer);
+        }
+
+    }
+
+    public void printStatement()
+    {
+        for (Customer customer: customers) {
+            System.out.print(customer.getName() + ": {");
+            for (double transaction : customer.getTransactions())
+            {
+                System.out.print(transaction + ", ");
+            }
+            System.out.println("}");
+        }
+    }
+}
