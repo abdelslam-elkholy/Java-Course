0c8f06d1d9f4fa3980fd58f1b3587a1da0bc1dbc
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-29 00:55:19 +0200

    Regex

--- BlackJack/src/main/java/com/blackjack/RegexPractise.java
+++ BlackJack/src/main/java/com/blackjack/RegexPractise.java
@@ -2,8 +2,10 @@ package com.blackjack;
 
 public class RegexPractise {
     public static void main(String[] args) {
-        System.out.println("jat".matches("[^a-fA-F]at"));
-        System.out.println("123-123-1234".matches("\\d\\d\\d-\\d\\d\\d-\\d\\d\\d\\d"));
+//        System.out.println("jat".matches("[^a-fA-F]at"));
+//        System.out.println("123-123-1234".matches("\\d\\d\\d-\\d\\d\\d-\\d\\d\\d\\d"));
+//        System.out.println("123-123-1234".matches("\\d{3}-\\d{3}-\\d{4}"));
+        System.out.println("123-123-1234".matches("\\d{3}[-,]\\d{3}[-,]\\d{4}"));
 
     }
 }
