35fbd70c72d5e6c04135ecdbc4591f01071780c2
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-28 17:41:48 +0200

    OOP

--- BlackJack/src/main/java/com/blackjack/Rank.java
+++ BlackJack/src/main/java/com/blackjack/Rank.java
@@ -1,26 +1,33 @@
 package com.blackjack;
 
 public enum Rank {
-    ACE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SEX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING;
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SEX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10);
+
+    private int value;
+
+    Rank(int value) {
+        this.value = value;
+    }
 
     public int getValue()
     {
-        return switch (this)
-        {
-            case JACK , KING ,QUEEN -> 10;
-            default -> ordinal() + 1;
-        };
+//        return switch (this)
+//        {
+//            case JACK , KING ,QUEEN -> 10;
+//            default -> ordinal() + 1;
+//        };
+        return value;
     }
 }
