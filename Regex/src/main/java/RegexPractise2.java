e8a0d9549192bdc74b4608622fce9cd7b6dbe23e
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-30 00:46:32 +0200

    regex

--- Regex/src/main/java/RegexPractise2.java
+++ Regex/src/main/java/RegexPractise2.java
@@ -14,11 +14,15 @@ public class RegexPractise2 {
                 Cumulative GPA (Unweighted)	3.46
                 """;
         String regex = """
-                    Student\\sNumber:\\s(?<studentNum>\\d{10}).*
-                    Grade:\\s+(?<grade>\\d{1,2}).*
-                    Birthdate:\\s+(?<birthdateDay>\\d{2})/(?<birthdateMonth>\\d{2})/(?<birthdateYear>\\d{4}).*
-                    
-                    """;
+        Student\\sNumber:\\s(?<studentNum>\\d{10}).*
+        Grade:\\s+(?<grade>\\d{1,2}).*
+        Birthdate:\\s+(?<birthdateDay>\\d{2})/(?<birthdateMonth>\\d{2})/(?<birthdateYear>\\d{4}).*
+        Gender:\\s+(?<gender>\\w).*
+        State\\sID:\\s+(?<stateId>\\d+).*
+        Weighted\\)\\s+(?<gpa>\\d+\\.\\w+)\\b.*
+        Unweighted\\)\\s+(?<gpa2>\\d+\\.\\w+)\\b.*
+
+        """;
         Pattern pat = Pattern.compile(regex , Pattern.DOTALL | Pattern.COMMENTS);
         Matcher mat = pat.matcher(transcript);
 
@@ -28,6 +32,11 @@ public class RegexPractise2 {
             System.out.println(mat.group("birthdateDay"));
             System.out.println(mat.group("birthdateMonth"));
             System.out.println(mat.group("birthdateYear"));
+            System.out.println(mat.group("gender"));
+            System.out.println(mat.group("stateId"));
+            System.out.println(mat.group("gpa"));
+            System.out.println(mat.group("gpa2"));
+
         }
     }
 }
