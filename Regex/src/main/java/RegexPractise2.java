86cbf282369e13bb6ca39eb43b248a75a553c036
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-29 21:46:43 +0200

    regex

--- Regex/src/main/java/RegexPractise2.java
+++ Regex/src/main/java/RegexPractise2.java
@@ -1,2 +1,33 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexPractise2 {
+    public static void main(String[] args) {
+//        System.out.println("dog".matches(".*"));
+
+        String transcript = """
+                Student Number:	1234598872			Grade:		11
+                Birthdate:		01/02/2000			Gender:	M
+                State ID:		8923827123
+                                
+                Cumulative GPA (Weighted)		3.82
+                Cumulative GPA (Unweighted)	3.46
+                """;
+        String regex = """
+                    Student\\sNumber:\\s(?<studentNum>\\d{10}).*
+                    Grade:\\s+(?<grade>\\d{1,2}).*
+                    Birthdate:\\s+(?<birthdateDay>\\d{2})/(?<birthdateMonth>\\d{2})/(?<birthdateYear>\\d{4}).*
+                    
+                    """;
+        Pattern pat = Pattern.compile(regex , Pattern.DOTALL | Pattern.COMMENTS);
+        Matcher mat = pat.matcher(transcript);
+
+        if(mat.matches()){
+            System.out.println(mat.group("studentNum"));
+            System.out.println(mat.group("grade"));
+            System.out.println(mat.group("birthdateDay"));
+            System.out.println(mat.group("birthdateMonth"));
+            System.out.println(mat.group("birthdateYear"));
+        }
+    }
 }
