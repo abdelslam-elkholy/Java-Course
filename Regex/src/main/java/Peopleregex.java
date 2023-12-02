9d734c08427438c01e81996d0d15d2fabda87633
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-12-02 14:17:17 +0200

    regex

---
+++ Regex/src/main/java/Peopleregex.java
@@ -0,0 +1,9 @@
+public class Peopleregex {
+    private String peoble = """
+              Adam, Ahmed, 1/1/1990
+              Mohhamed, Mahmoud, 1/10/1993
+              Hend, Ahmed, 25/10/1995  
+              """;
+    String regex =
+
+}
