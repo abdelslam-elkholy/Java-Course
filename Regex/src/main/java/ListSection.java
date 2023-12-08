10f745911b78d019b93bf7f17dbe08d61ef8262f
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-12-08 14:23:45 +0200

    update

---
+++ Regex/src/main/java/ListSection.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSection {
+
+    public static void main(String[] args) {
+        List<IEmpolyee> arrayList = new ArrayList<>();
+
+    }}
+
+interface IEmpolyee
+{
+
+}
