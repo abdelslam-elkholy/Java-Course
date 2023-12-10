75e764c2362742d25bc30b65ef922d1c4bd851d9
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-12-10 15:04:23 +0200

    Collections

---
+++ collections/src/main/java/ExerciseOne.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExerciseOne {
+    public static void main(String[] args) {
+
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Toyota" , "Corolla" , Color.BLACK));
+        cars.add(new Car("Toyota" , "Corolla" , Color.BLUE));
+        cars.add(new Car("Toyota" , "Corolla" , Color.RED));
+
+        for (Car car: cars)
+        {
+            System.out.println(car);
+        }
+    }
+}
+
+record Car(  String brand, String model, Color color) {
+    @Override
+    public String toString() {
+        return "Brand new " + color.toString().charAt(0)+color.toString().substring(1).toLowerCase()+ " " +brand+" "+ model ;
+    }
+}
+
+enum Color {
+    RED,
+    BLUE,
+    WHITE,
+    BLACK
+}
