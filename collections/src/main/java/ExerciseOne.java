317ed3d495761e568fec8828dbb985b67c9253b3
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-12-10 15:20:53 +0200

    Collections

--- collections/src/main/java/ExerciseOne.java
+++ collections/src/main/java/ExerciseOne.java
@@ -1,20 +1,32 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class ExerciseOne {
     public static void main(String[] args) {
 
 //        List<Car> cars = new ArrayList<>();
-        Set<Car> cars = new HashSet<>();
+        Set<Car> cars = new LinkedHashSet<>();
+        List<String> names = new ArrayList<>(List.of("Ahmed" , "Mohammed" , "Mahmoud"));
+        Map<String , Car> table = new HashMap<>();
+
+
         cars.add(new Car("Toyota" , "Corolla" , Color.BLACK));
         cars.add(new Car("Toyota" , "Corolla" , Color.BLACK));
         cars.add(new Car("Toyota" , "Corolla" , Color.BLUE));
         cars.add(new Car("Toyota" , "Corolla" , Color.RED));
 
+        int i =0;
+        for (Car car: cars)
+        {
+            table.putIfAbsent(names.get(i) , car);
+            i++;
+        }
+
         for (Car car: cars)
         {
             System.out.println(car);
+
         }
+        System.out.println(table);
     }
 }
 
