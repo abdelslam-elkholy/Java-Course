62c723205b7cb09ee89f6e097237b12b5cba8267
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-12-16 06:21:24 +0200

    JDBC

--- PeopleDB/src/test/java/com/abdelslam/peopledb/repository/PeopleRepositoryTests.java
+++ PeopleDB/src/test/java/com/abdelslam/peopledb/repository/PeopleRepositoryTests.java
@@ -10,10 +10,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PeopleRepositoryTests {
     @Test
-    void canSave() {
+    void canSaveOnePerson() {
         PeopleRepository repo = new PeopleRepository();
         Person john = new Person("john" , "smith" , ZonedDateTime.of(1990,11,15,15,15,15,0, ZoneId.of("-6")));
         Person savedPerson = repo.save(john);
         assertThat(savedPerson.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    void canSaveTowPeople() {
+        PeopleRepository repo = new PeopleRepository();
+        Person john = new Person("john" , "smith" , ZonedDateTime.of(1990,11,15,15,15,15,0, ZoneId.of("-6")));
+        Person bobby = new Person("bobby" , "smith" , ZonedDateTime.of(1992,1,25,15,15,15,0, ZoneId.of("-6")));
+        Person savedPerson1 = repo.save(john);
+        Person savedPerson2 = repo.save(bobby);
+        assertThat(savedPerson1.getId()).isNotEqualTo(savedPerson2.getId());
     }
 }
