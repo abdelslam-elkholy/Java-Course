3a051d494a143b5596128da45f8d90d5d0cbe2dc
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-12-16 06:10:56 +0200

    JDBC

--- PeopleDB/src/main/java/com/abdelslam/peopledb/model/Person.java
+++ PeopleDB/src/main/java/com/abdelslam/peopledb/model/Person.java
@@ -3,6 +3,11 @@ package com.abdelslam.peopledb.model;
 import java.time.ZonedDateTime;
 
 public class Person {
+    private long id;
     public Person(String firstName, String lastName, ZonedDateTime dob) {
+    }
+
+    public long getId() {
+        return 1L;
     }
 }
--- PeopleDB/src/test/java/com/abdelslam/peopledb/repository/PeopleRepositoryTests.java
+++ PeopleDB/src/test/java/com/abdelslam/peopledb/repository/PeopleRepositoryTests.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PeopleRepositoryTests {
     @Test
     void canSave() {
         PeopleRepository repo = new PeopleRepository();
         Person john = new Person("john" , "smith" , ZonedDateTime.of(1990,11,15,15,15,15,0, ZoneId.of("-6")));
-        person savedPerson repo.save(john);
+        Person savedPerson = repo.save(john);
+        assertThat(savedPerson.getId()).isGreaterThan(0);
     }
 }
