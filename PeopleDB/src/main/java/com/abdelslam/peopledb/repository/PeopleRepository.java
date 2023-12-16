c3a41d5d9808926875306e6d78bf25faa57eab85
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-12-16 06:33:04 +0200

    JDBC

--- PeopleDB/src/main/java/com/abdelslam/peopledb/repository/PeopleRepository.java
+++ PeopleDB/src/main/java/com/abdelslam/peopledb/repository/PeopleRepository.java
@@ -2,7 +2,14 @@ package com.abdelslam.peopledb.repository;
 
 import com.abdelslam.peopledb.model.Person;
 
+import java.sql.Connection;
+
 public class PeopleRepository {
+    private Connection connection;
+    public PeopleRepository(Connection connection) {
+        this.connection = connection;
+    }
+
     public Person save(Person person) {
         return person;
     }
--- PeopleDB/src/test/java/com/abdelslam/peopledb/repository/PeopleRepositoryTests.java
+++ PeopleDB/src/test/java/com/abdelslam/peopledb/repository/PeopleRepositoryTests.java
@@ -3,6 +3,9 @@ package com.abdelslam.peopledb.repository;
 import com.abdelslam.peopledb.model.Person;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
@@ -10,8 +13,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PeopleRepositoryTests {
     @Test
-    void canSaveOnePerson() {
-        PeopleRepository repo = new PeopleRepository();
+    void canSaveOnePerson() throws SQLException {
+        Connection connection = DriverManager.getConnection("/home/abdelslam/Desktop/Courses/Java/people-test");
+        PeopleRepository repo = new PeopleRepository(connection);
         Person john = new Person("john" , "smith" , ZonedDateTime.of(1990,11,15,15,15,15,0, ZoneId.of("-6")));
         Person savedPerson = repo.save(john);
         assertThat(savedPerson.getId()).isGreaterThan(0);
