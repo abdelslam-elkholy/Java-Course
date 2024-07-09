fac1475fe3a445c3178eb5ae606903b073ef9666
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-07-09 23:11:51 +0300

    Adding basic tests

---
+++ PeopleDBS/src/test/java/com/abdelslam/peopledbs/repository/PeopleRepository.java
@@ -0,0 +1,4 @@
+package com.abdelslam.peopledbs.repository;
+
+public class PeopleRepository {
+}
---
+++ PeopleDBS/src/test/java/com/abdelslam/peopledbs/repository/PeopleRepositoryTests.java
@@ -0,0 +1,11 @@
+package com.abdelslam.peopledbs.repository;
+
+import org.junit.jupiter.api.Test;
+
+public class PeopleRepositoryTests {
+@Test
+    public void canSave(){
+    PeopleRwepository repo = new PeopleRepository()
+}
+
+}
