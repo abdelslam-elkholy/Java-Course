37e3af08598a2343f3a27b2cb70ee22a7d80cec0
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-08-02 08:43:36 +0300

    interface

--- InterFaces/src/Product.java
+++ InterFaces/src/Product.java
@@ -19,10 +19,27 @@ class Monitor extends Product {
 }
 
 class MotherBoard extends Product {
+    private int ramSlots;
+    private int cardSlots;
+    private String bios;
+
+
     public MotherBoard(String model, String manufactuerer) {
         super(model, manufactuerer);
     }
+
+    public MotherBoard(String model, String manufactuerer, int ramSlots, int cardSlots, String bios) {
+        super(model, manufactuerer);
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
+
+    public void loadProgram(String programName) {
+        System.out.printf("Program %d is now loading...\n" , programName);
+    }
 }
+
 class ComputerCase extends Product {
     public ComputerCase(String model, String manufactuerer) {
         super(model, manufactuerer);
