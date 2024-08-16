9220da07cdbe696d1e374bb6c9c9bd50635b78cc
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-08-16 18:35:21 +0300

    polymorphysm

--- Movie/src/Movie.java
+++ Movie/src/Movie.java
@@ -2,11 +2,26 @@ public class Movie {
     private String title;
 
     public Movie(String title) {
-//        this.title = title;
+        this.title = title;
     }
 
     public void watchMovie() {
         String instanceType = this.getClass().getSimpleName();
         System.out.println(title + " is a " + instanceType + " film");
+    }
+}
+
+class Adventure extends Movie {
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("...%s%n".repeat(3) ,
+                "pleasant Scene" ,
+                "Scary Music" ,
+                "Something scary happend" );
     }
 }
